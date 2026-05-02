79e4cf36b3641a6b5f8aad4d79cf14c8ab67152d
Aryan Mishra <aryan456.rehla@gmail.com>
2026-05-02 11:36:40 +0530

    docker file

--- temp.cpp
+++ temp.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Execution successful. Hello from the Node.js bridge." << endl;
+    while(true){
+        cout << "Execution successful. Hello from the Node.js bridge." << endl;
+    }
     return 0;
 }
