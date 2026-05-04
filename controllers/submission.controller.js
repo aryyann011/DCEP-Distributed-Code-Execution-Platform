774ad460d6e22282afe6e8d7d78a5c1c3d1f9cae
Aryan Mishra <aryan456.rehla@gmail.com>
2026-05-04 15:11:50 +0530

    docker installation and api endpoint for code compilation

--- controllers/submission.controller.js
+++ controllers/submission.controller.js
@@ -1,6 +1,7 @@
 import { writeFileSync } from 'fs';
 import { exec } from 'child_process';
 import util from 'util';
+import crypto from 'crypto'; 
 
 const execPromise = util.promisify(exec);
 
@@ -8,34 +9,27 @@ export const RunTheCode = async (req, res) => {
     try {
         const code = req.body.code;
 
-        const fileName = 'temp.cpp';
-        const outputName = 'temp.out';
+        const jobId = crypto.randomUUID();
+        const fileName = `${jobId}.cpp`;
+        const outputName = `${jobId}.out`;
 
-        console.log("Writing file...");
+        console.log(`[${jobId}] Writing file...`);
         writeFileSync(fileName, code);
 
-        console.log("Compiling...");
+        console.log(`[${jobId}] Compiling...`);
         await execPromise(`g++ ${fileName} -o ${outputName}`);
 
-        console.log("Running...");
-        const { stdout, stderr } = await execPromise(`${outputName}`);
+        console.log(`[${jobId}] Running...`);
+        const { stdout, stderr } = await execPromise(`./${outputName}`);
 
         if (stderr) {
-            return res.status(400).json({
-                success: false,
-                error: stderr
-            });
+            return res.status(400).json({ success: false, error: stderr });
         }
 
-        return res.status(200).json({
-            success: true,
-            output: stdout
-        });
+        return res.status(200).json({ success: true, output: stdout });
 
     } catch (err) {
-        return res.status(500).json({
-            success: false,
-            error: err.message
-        });
+        const errorMessage = err.stderr ? err.stderr : err.message;
+        return res.status(500).json({ success: false, error: errorMessage });
     }
 };
