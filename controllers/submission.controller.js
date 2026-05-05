4e54e46744cd33900b6afb8f6fb360168b543cf3
Aryan Mishra <aryan456.rehla@gmail.com>
2026-05-06 00:55:37 +0530

    mini changes

--- controllers/submission.controller.js
+++ controllers/submission.controller.js
@@ -1,35 +1,47 @@
 import { writeFileSync } from 'fs';
-import { exec } from 'child_process';
-import util from 'util';
-import crypto from 'crypto'; 
-
-const execPromise = util.promisify(exec);
+import crypto from 'crypto';
+import Docker from 'dockerode'; 
+const docker = new Docker();
 
 export const RunTheCode = async (req, res) => {
     try {
         const code = req.body.code;
-
         const jobId = crypto.randomUUID();
         const fileName = `${jobId}.cpp`;
         const outputName = `${jobId}.out`;
 
-        console.log(`[${jobId}] Writing file...`);
+        console.log(`[${jobId}] Writing file to host...`);
         writeFileSync(fileName, code);
 
-        console.log(`[${jobId}] Compiling...`);
-        await execPromise(`g++ ${fileName} -o ${outputName}`);
+        const currentDirectory = process.cwd();
 
-        console.log(`[${jobId}] Running...`);
-        const { stdout, stderr } = await execPromise(`./${outputName}`);
+        console.log(`[${jobId}] Spinning up Docker Sandbox...`);
 
-        if (stderr) {
-            return res.status(400).json({ success: false, error: stderr });
-        }
+        const container = await docker.createContainer({
+            Image: 'cpp-sandbox',
+            Tty: false,
+            Cmd: ['sh', '-c', `g++ ${fileName} -o ${outputName} && ./${outputName}`],
+            HostConfig: {
+                Binds: [`${currentDirectory}:/app`], 
+                Memory: 256 * 1024 * 1024,           
+                NetworkMode: 'none',                 
+            }
+        });
 
-        return res.status(200).json({ success: true, output: stdout });
+        await container.start();
+
+        await container.wait();
+
+        const logs = await container.logs({ stdout: true, stderr: true });
+        
+        const output = logs.toString('utf-8').replace(/[^\x20-\x7E\n]/g, '').trim();
+
+        console.log(`[${jobId}] Destroying Sandbox...`);
+        await container.remove();
+
+        return res.status(200).json({ success: true, output: output });
 
     } catch (err) {
-        const errorMessage = err.stderr ? err.stderr : err.message;
-        return res.status(500).json({ success: false, error: errorMessage });
+        return res.status(500).json({ success: false, error: err.message });
     }
 };
