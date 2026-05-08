44c757dac19bd6f34a12ab817faab165f5cec8f2
Aryan Mishra <aryan456.rehla@gmail.com>
2026-05-08 15:13:20 +0530

    BullMQ worker setup

--- controllers/submission.controller.js
+++ controllers/submission.controller.js
@@ -1,76 +1,53 @@
-import { Queue } from 'bullmq';
-import crypto from 'crypto'
-const myQueue = new Queue('foo');
+import { Worker } from "bullmq";
+import Docker from 'dockerode';
 import { writeFileSync } from 'fs';
+import IORedis from 'ioredis';
 
-export const RunTheCode = async (req, res) => {
-    try {
-        const jobId = crypto.randomUUID()
-        const code = req.body.code
-        const fileName = `{jobId}.cpp`
-        const outputName = `${jobId}.out`
+const connection = new IORedis({ host : '127.0.0.1', port : 6379, maxRetriesPerRequest : null });
+const docker = new Docker();
 
-        console.log(`[${jobId}] Writing file to host...`);
+console.log("Worker is online listening to submission queue");
+
+const worker = new Worker(
+    'submissions', 
+    async job => {
+        const code = job.data.code;
+        const jobId = job.opts.jobId;
+        
+        const fileName = `${jobId}.cpp`;
+        const outputName = `${jobId}.out`;
+        
         writeFileSync(fileName, code);
-
+        console.log(`[${jobId}] Processing code:\n${code}`);
         const currentDirectory = process.cwd();
 
-        await myQueue.add('myC++Code', { code : code });
+        console.log(`[${jobId}] Spinning up Docker Sandbox...`);
 
-        return res.status(200).json({
-            success : true,
-            message : "code successfully queued"
-        })
+        const container = await docker.createContainer({
+            Image: 'cpp-sandbox',
+            Tty: false,
+            Cmd: ['sh', '-c', `g++ ${fileName} -o ${outputName} && ./${outputName}`],
+            HostConfig: {
+                Binds: [`${currentDirectory}:/app`], 
+                Memory: 256 * 1024 * 1024,           
+                NetworkMode: 'none',                 
+            }
+        });
 
-    } catch (error) {
-        
-    }
-}
+        await container.start();
+        await container.wait();
 
-// import { writeFileSync } from 'fs';
-// import crypto from 'crypto';
-// import Docker from 'dockerode'; 
-// const docker = new Docker();
+        const logs = await container.logs({ stdout: true, stderr: true });
+        const output = logs.toString('utf-8').replace(/[^\x20-\x7E\n]/g, '').trim();
 
-// export const RunTheCode = async (req, res) => {
-//     try {
-//         const code = req.body.code;
-//         const jobId = crypto.randomUUID();
-//         const fileName = `${jobId}.cpp`;
-//         const outputName = `${jobId}.out`;
+        console.log(`[${jobId}] Destroying Sandbox...`);
+        await container.remove();
 
-//         console.log(`[${jobId}] Writing file to host...`);
-//         writeFileSync(fileName, code);
+        return output;
+    },
+    { connection }
+);
 
-//         const currentDirectory = process.cwd();
-
-//         console.log(`[${jobId}] Spinning up Docker Sandbox...`);
-
-//         const container = await docker.createContainer({
-//             Image: 'cpp-sandbox',
-//             Tty: false,
-//             Cmd: ['sh', '-c', `g++ ${fileName} -o ${outputName} && ./${outputName}`],
-//             HostConfig: {
-//                 Binds: [`${currentDirectory}:/app`], 
-//                 Memory: 256 * 1024 * 1024,           
-//                 NetworkMode: 'none',                 
-//             }
-//         });
-
-//         await container.start();
-
-//         await container.wait();
-
-//         const logs = await container.logs({ stdout: true, stderr: true });
-        
-//         const output = logs.toString('utf-8').replace(/[^\x20-\x7E\n]/g, '').trim();
-
-//         console.log(`[${jobId}] Destroying Sandbox...`);
-//         await container.remove();
-
-//         return res.status(200).json({ success: true, output: output });
-
-//     } catch (err) {
-//         return res.status(500).json({ success: false, error: err.message });
-//     }
-// };
+worker.on('failed', (job, error)=>{
+    console.log(`[${job?.opts?.jobId}] failed due to the error ${error.message}`);
+});
---
+++ worker.js
@@ -0,0 +1,59 @@
+import { Worker } from "bullmq";
+import Docker from 'dockerode'
+import {writeFilesync} from 'fs'
+import IORedis from 'ioredis';
+
+const connection = new IORedis({ host : '127.0.0.1', port : 6379, maxRetriesPerRequest : null });
+const docker = new Dockerode()
+console.log("Worker is online listening to submission queue")
+
+export const RunTheCode = () => {
+    const jobId = crypto.randomUUID()
+    const fileName = `{jobId}.cpp`
+    const outputName = `${jobId}.out`
+
+    console.log(`[${jobId}] Writing file to host...`);
+    const worker = new Worker(
+    'compile-job',
+    async job => {
+        const code = job.data.code
+        const jobId = job.opts.jobId
+        const fileName = `{jobId}.cpp`
+        const outputName = `${jobId}.out`
+        writeFileSync(fileName, code);
+        console.log(code);
+        const currentDirectory = process.cwd();
+
+        console.log(`[${jobId}] Spinning up Docker Sandbox...`);
+
+        const container = await docker.createContainer({
+            Image: 'cpp-sandbox',
+            Tty: false,
+            Cmd: ['sh', '-c', `g++ ${fileName} -o ${outputName} && ./${outputName}`],
+            HostConfig: {
+                Binds: [`${currentDirectory}:/app`], 
+                Memory: 256 * 1024 * 1024,           
+                NetworkMode: 'none',                 
+            }
+        });
+
+        await container.start();
+
+        await container.wait();
+
+        const logs = await container.logs({ stdout: true, stderr: true });
+        
+        const output = logs.toString('utf-8').replace(/[^\x20-\x7E\n]/g, '').trim();
+
+        console.log(`[${jobId}] Destroying Sandbox...`);
+        await container.remove();
+
+        return output;
+    },
+    { connection },
+    );
+
+    worker.on('failed', (job, error)=>{
+        console.log(`[${job?.opts?.jobId}] failed due to the error ${error.message}`);
+    });
+}
