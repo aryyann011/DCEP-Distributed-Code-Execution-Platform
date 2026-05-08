2a1b6132b05341934f487663803cf976dedb095e
Aryan Mishra <aryan456.rehla@gmail.com>
2026-05-09 03:45:36 +0530

    docker redis worker connection settled

--- worker.js
+++ worker.js
@@ -1,27 +1,24 @@
 import { Worker } from "bullmq";
-import Docker from 'dockerode'
-import {writeFilesync} from 'fs'
+import Docker from 'dockerode';
+import { writeFileSync } from 'fs';
 import IORedis from 'ioredis';
 
 const connection = new IORedis({ host : '127.0.0.1', port : 6379, maxRetriesPerRequest : null });
-const docker = new Dockerode()
-console.log("Worker is online listening to submission queue")
+const docker = new Docker();
 
-export const RunTheCode = () => {
-    const jobId = crypto.randomUUID()
-    const fileName = `{jobId}.cpp`
-    const outputName = `${jobId}.out`
+console.log("Worker is online listening to submission queue");
 
-    console.log(`[${jobId}] Writing file to host...`);
-    const worker = new Worker(
-    'compile-job',
+const worker = new Worker(
+    'submissions', 
     async job => {
-        const code = job.data.code
-        const jobId = job.opts.jobId
-        const fileName = `{jobId}.cpp`
-        const outputName = `${jobId}.out`
+        const code = job.data.code;
+        const jobId = job.opts.jobId;
+        
+        const fileName = `${jobId}.cpp`;
+        const outputName = `${jobId}.out`;
+        
         writeFileSync(fileName, code);
-        console.log(code);
+        console.log(`[${jobId}] Processing code:\n${code}`);
         const currentDirectory = process.cwd();
 
         console.log(`[${jobId}] Spinning up Docker Sandbox...`);
@@ -38,11 +35,9 @@ export const RunTheCode = () => {
         });
 
         await container.start();
-
         await container.wait();
 
         const logs = await container.logs({ stdout: true, stderr: true });
-        
         const output = logs.toString('utf-8').replace(/[^\x20-\x7E\n]/g, '').trim();
 
         console.log(`[${jobId}] Destroying Sandbox...`);
@@ -50,10 +45,9 @@ export const RunTheCode = () => {
 
         return output;
     },
-    { connection },
-    );
+    { connection }
+);
 
-    worker.on('failed', (job, error)=>{
-        console.log(`[${job?.opts?.jobId}] failed due to the error ${error.message}`);
-    });
-}
+worker.on('failed', (job, error)=>{
+    console.log(`[${job?.opts?.jobId}] failed due to the error ${error.message}`);
+});
